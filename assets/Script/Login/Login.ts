4fe3d8a2f9d4a87122e4738afa9ae2a7b090c52c
iluspan <411878396@qq.com>
2022-10-18 16:27:37 +0800

    安卓sdk接入

--- assets/Script/Login/Login.ts
+++ assets/Script/Login/Login.ts
@@ -6,22 +6,31 @@
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
 
 import { findNode } from "../Common/GlobalFuncs";
+import { LoadingPanelMgr } from "../managers/LoadingPanelMgr";
+import { SdkManager } from "../managers/SdkManager";
+import { TipMgr } from "../managers/TipMgr";
 import CptBase from "../MsgFrame/CptBase";
-import { Msg, MsgCmd} from "../MsgFrame/Msg";
+import { Msg, MsgCmd } from "../MsgFrame/Msg";
 import { MsgCenter } from "../MsgFrame/MsgCenter";
 import { WebSocketEx } from "../MsgFrame/WebSocketEx";
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Login extends CptBase {
+    @property(cc.Prefab)
+    gameTip: cc.Prefab = null;
 
-    SetSupportMainCmd():number[]{
+    @property(cc.Prefab)
+    loadingPanel: cc.Prefab = null;
+
+    SetSupportMainCmd(): number[] {
         return [MsgCmd.Login];
     }
 
     protected onLoad(): void {
         super.onLoad();
+
     }
 
     protected onDestroy(): void {
@@ -29,14 +38,15 @@ export default class Login extends CptBase {
 
     }
 
-    start () {
+    start() {
         let btnNameNode = findNode('title', this.node);
-        btnNameNode.getComponent(cc.Label).string = '登录';
+        // btnNameNode.getComponent(cc.Label).string = '登录';
+        TipMgr.ins().initUI(this.gameTip, this.gameTip);
+        LoadingPanelMgr.ins().init(this.loadingPanel);
     }
 
     RecvMsg(msg: Msg): void {
-        switch (msg.mainCmd)
-        {
+        switch (msg.mainCmd) {
             case MsgCmd.Login:
                 {
                     if (0x01 == msg.subCmd) { //个人消息
@@ -49,9 +59,24 @@ export default class Login extends CptBase {
         }
     }
 
-    Login(){
+    Login() {
         // WebSocketEx.GetInstance().connect("", null);
-        MsgCenter.SendCustomMsg(MsgCmd.Login, 1, null, 0, null);
+        // MsgCenter.SendCustomMsg(MsgCmd.Login, 1, null, 0, null);
+    }
+
+    private onLoginBtnHandler(): void {
+        let self = this;
+        SdkManager.ins().loginAuthorization(self.loginCallbackHandler.bind(self));
+    }
+
+    private loginCallbackHandler(data): void {
+        let self = this;
+        LoadingPanelMgr.ins().removeLoadingPanel();
+        if (data) {
+            cc.director.loadScene("Game");
+        } else {
+            cc.director.loadScene("Game");
+        }
     }
 
     // update (dt) {}
